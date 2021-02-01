fbb5ff4593cf2f3f1423288b12e65030cfc441ce
Mateus Furquim <furquim.axs@gmail.com>
2021-01-31 21:23:58 -0300

    Send and receive message

---
+++ send.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello,')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='World!')
+
+print("[x] Sent 'Hello World!'")
+
+connection.close()
